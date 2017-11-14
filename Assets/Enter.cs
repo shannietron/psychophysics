61c696448ad52ce9606e3b8f2adcc23ff7783361
shannietron <git@shan.mp>
2017-11-14 18:19:52 -0500

    Add Countdown timer to destroy sphere

--- Assets/Enter.cs
+++ Assets/Enter.cs
@@ -4,32 +4,45 @@ using UnityEngine;
 
 
 public class Enter : MonoBehaviour {
+    public static bool success = false;
     private string filePath = "D:/mycoords.txt";
     private string coordinates = "";
+    private static int numFrames = 45; //number of frames to capture @ 90fps
+    private int count = 0;
 
 
     private void OnTriggerEnter(Collider other)
     {
         GetComponent<Renderer>().material.color = Color.red;
         Debug.Log(gameObject.name +"was triggered by!"+other.gameObject.name);
+        coordinates = "";
+        count = 0;
 
     }
     private void OnTriggerExit(Collider other)
     {
         GetComponent<Renderer>().material.color = Color.green;
         Debug.Log(gameObject.name + "was triggered by!" + other.gameObject.name);
-
+        coordinates = "";
+        count = 0;
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (other.attachedRigidbody)
-            coordinates = "";
-            Transform trans = other.transform;
-            coordinates += "x: " + trans.position.x.ToString() + ", y: " + trans.position.y.ToString() + ", z: " + trans.position.z.ToString() + System.Environment.NewLine;
+        count++;
+        Debug.Log("count:" + count);
+            
+        Transform trans = other.transform;
+        coordinates += "x: " + trans.position.x.ToString() + ", y: " + trans.position.y.ToString() + ", z: " + trans.position.z.ToString() + ", time: " + Time.deltaTime.ToString() + System.Environment.NewLine;
+        if (count == 45)
+        {
+            success = true;
             System.IO.File.WriteAllText(filePath, coordinates);
+            coordinates = "";
+            count = 0;
+            Object.Destroy(this.gameObject);
 
-
+        }
     }
 
 
---
+++ Assets/countDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countDown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Countdown", 5);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+		
+	}
+
+    private IEnumerator Countdown(int time)
+    {
+        while (time > 0)
+        {
+            Debug.Log(time--);
+            yield return new WaitForSeconds(1);
+        }
+        Debug.Log("Countdown Complete!");
+        Object.Destroy(this.gameObject);
+
+    }
+}
